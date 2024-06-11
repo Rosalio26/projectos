9e7236fb02f711b1392a84ecb9de12cf7d750a48
Rosalio26 <rosalioalexandre26@gmail.com>
2024-06-11 14:08:06 +0200

    ch#cpy001

--- caracol/script/apt.js
+++ caracol/script/apt.js
@@ -1,1 +1,13 @@
 
+function btnmenuHamb() {
+    var myNav = document.querySelector('.itm-content');
+    var mynavBar = document.querySelector('.navBar');
+
+    if(myNav.style.display === "none" || mynavBar.style.backgroundColor === "transparent") {
+        myNav.style.display = "block";
+        mynavBar.style.backgroundColor = "blue"
+    } else {
+        myNav.style.display = "none";
+        mynavBar.style.backgroundColor = "transparent"
+    }
+}
