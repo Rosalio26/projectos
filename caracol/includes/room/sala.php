7e4ffe7a0736eaa9c08e08dda6ca38423ecc80e3
Rosalio26 <rosalioalexandre26@gmail.com>
2024-06-13 16:04:54 +0200

    ch#00012

--- caracol/includes/room/sala.php
+++ caracol/includes/room/sala.php
@@ -2,7 +2,21 @@
    require "../header.php";
 ?>
 
-<div>
-    <div></div>
-    <div></div>
+<?php 
+  $cssFile ="../style/sala.css";
+  echo "<link rel='stylesheet' href='" . $cssFile . "'>";
+?>
+
+<div class="content row">
+    <div class="grid-container">
+        <div class="left">
+            <h2>Sala</h2>
+            <div class="container">
+            </div>
+        </div>
+        <div class="right">
+            <div class="top">column 3.1</div>
+            <div class="bottom">column  3.2</div>
+        </div>
+    </div>
 </div>
