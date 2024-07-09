03e876d68c0327a2885bc36126beb3af469ec15c
Rosalio26 <rosalioalexandre26@gmail.com>
2024-07-10 00:35:55 +0200

    res#15-21

---
+++ caracol/ownless/othless/other_room.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sites | Cursos de tecnologias</title>
+</head>
+<body>
+    <section>
+        <div>
+            <p>Está pagina não se encotra a desposição de conteudos ditos. <br> Nossas siceras desculpes.</p>
+            <p>Estamos a trabalhar para poder restaurar o mesmo.</p>
+        </div>
+    </section>
+</body>
+</html>
