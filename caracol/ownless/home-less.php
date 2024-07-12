c25288349629d5bf6dd7327754d6d792d65b5d25
Rosalio26 <rosalioalexandre26@gmail.com>
2024-07-12 02:13:45 +0200

    ch#00100

--- caracol/ownless/home-less.php
+++ caracol/ownless/home-less.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        body{
-            background-color: red;
-        }
-    </style>
+    <title>Sala Caracolearn</title>
+    <link rel="stylesheet" href="./style/home_less.css">
 </head>
-<body>
-    
+<body class="home-less">
+    <div class="cntet-rgh-bd bdy-cnt-inc">
+        <header class="header">
+        </header>
+        <main class="main">
+        </main>
+    </div>
+    <div class="fotr-min-bdy bdy-cnt-inc">
+    </div>
 </body>
 </html>
