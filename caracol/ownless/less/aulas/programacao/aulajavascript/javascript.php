91dc51b34425d574aa1580e703ed238e78b61b62
Rosalio26 <rosalioalexandre26@gmail.com>
2024-08-02 14:19:17 +0300

    add

--- caracol/ownless/less/aulas/programacao/aulajavascript/javascript.php
+++ caracol/ownless/less/aulas/programacao/aulajavascript/javascript.php
@@ -1,1 +1,21 @@
-<h1 id="javascript">Aula de JavaScript</h1>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula de JavaScript</title>
+    <link rel="stylesheet" href="./style/lessStyle.css">
+    <link rel="stylesheet" href="../../../style/lessStyle.css">
+</head>
+<body>
+    <h1 id="javascript">Aula de JavaScript</h1>
+
+    <nav class="cnt-less-itm">
+        <ul>
+            <li>Introdução</li>
+        </ul>
+    </nav>
+</body>
+</html>
--- caracol/ownless/less/room-lesson.php
+++ caracol/ownless/less/room-lesson.php
@@ -12,21 +12,24 @@
         <div class="cop-static">
             <div class="header-static">
                 <div class="logo-cnt-room">
-                    <a class="logo">
-                        <img src="" alt="imagem Logo">
-                        <span class="txt-lg-room">CaracoLearn</span>
-                    </a>
-                    <div class="bfs-room">
-                        <ul>
-                            <li>Biblioteca</li>
-                            <li>Ficheeiros</li>
-                            <li>Studdio</li>
-                        </ul>
+                    <div class="lef-itm">
+                        <a class="logo">
+                            <img src="" alt="imagem Logo">
+                            <span class="txt-lg-room">CaracoLearn</span>
+                        </a>
+                        <div class="bfs-room">
+                            <ul>
+                                <li>Biblioteca</li>
+                                <li>Ficheeiros</li>
+                                <li>Studdio</li>
+                            </ul>
+                        </div>
                     </div>
                     <div class="accout-room">
                         <span>Conta</span>
                     </div>
                 </div>
+
                 <div class="menu-cnt-room">
                     <div class="scrType">
                         <?php 
@@ -49,51 +52,50 @@
                             }
                         ?> 
                     </div>
-                    <a class="prev" onclick="plusSlides(-1, 0)">&#10094;</a>
-                    <a class="next" onclick="plusSlides(1, 0)">&#10095;</a>
                 </div>
             </div>
+
             <div class="main-itm-tw">
                 <div class="col-mn-itm-on">
-                    <div class="tlt-less">
-                    </div>
                     <div class="tlt-abd-tm">
-                    <?php 
-                       switch(@$_REQUEST["pages"]) {
-                        case "areajavascript";
-                            include("./aulas/programacao/aulajavascript/javascript.php");
-                        break;
+                        <?php 
+                        switch(@$_REQUEST["pages"]) {
+                            case "areajavascript";
+                                include("./aulas/programacao/aulajavascript/javascript.php");
+                            break;
 
-                        case "areajava";
-                            include("./aulas/programacao/aulajava/java.php");
-                        break;
+                            case "areajava";
+                                include("./aulas/programacao/aulajava/java.php");
+                            break;
 
-                        case "areaphp";
-                            include("./aulas/programacao/aulaphp/al_php.php");
-                        break;
+                            case "areaphp";
+                                include("./aulas/programacao/aulaphp/al_php.php");
+                            break;
 
-                        case "areacshp";
-                            include("./aulas/programacao/aulaLgCshp/lg_csh.php");
-                        break;
+                            case "areacshp";
+                                include("./aulas/programacao/aulaLgCshp/lg_csh.php");
+                            break;
+                            
+                            case "areacps";
+                                include("./aulas/programacao/aulaLgCplus/lg_Cpls.php");
+                            break;
+
+                            case "areacc";
+                                include("./aulas/programacao/aulaLg-c/lg_c.php");
+                            break;    
+
+                            default:
+                            print 'Nenhum Dado Foi captado!'
+                            ;
+                        }
+                        ?>
                         
-                        case "areacps";
-                            include("./aulas/programacao/aulaLgCplus/lg_Cpls.php");
-                        break;
-
-                        case "areacc";
-                            include("./aulas/programacao/aulaLg-c/lg_c.php");
-                        break;    
-
-                        default:
-                        print 'Nenhum Dado Foi captado!'
-                        ;
-                       }
-                    ?>
+                        <div style="height: 100%; width: 80%; background: red;" class="extra"> Estra</div>
                     </div>
                 </div>
+                
                 <div class="col-mn-itm-tw"></div>
             </div>
-            <div class="footer-grl-static">Por do sol</div>
         </div>
     </div>
 </body>
