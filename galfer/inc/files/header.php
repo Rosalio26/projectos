121a9fd22eb018d1c000810b8bcfc81dbfeb8d66
Rosalio26 <rosalioalexandre26@gmail.com>
2024-08-13 14:52:13 +0300

    #dd

---
+++ galfer/inc/files/header.php
@@ -0,0 +1,4 @@
+
+<div class="headerGeneral">
+
+</div>
---
+++ galfer/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomeTysip</title>
+</head>
+<body>
+    <?php include("./inc/files/header.php");?>
+
+    <div class="investTemplate">
+        <div class="dataFriends"></div>
+        <div class="dataBeaut"></div>
+        <div class="dataDream"></div>
+        <div class="dataStory"></div>
+        <div class="dataLove"></div>
+    </div>
+</body>
+</html>
