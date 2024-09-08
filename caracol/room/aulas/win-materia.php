78f90fc682289843bfa735c492b5eb58112ca30b
Rosalio26 <rosalioalexandre26@gmail.com>
2024-09-08 21:59:01 +0300

    update

--- caracol/room/aulas/win-materia.php
+++ caracol/room/aulas/win-materia.php
@@ -6,11 +6,38 @@
     <title>Materias Preparados || Inicio de Aula</title>
     <link rel="stylesheet" href="../conf/style/sala-materias.css">
     <link rel="stylesheet" href="../../style/mainHd.css">
+    <link rel="stylesheet" href="./conf_lesson/style/win-materias.css">
 </head>
-<body>
+<body class="win-materias">
     <header>
-        <?php include("../inc-sala-materias/header.php");?>
+        <?php include("../inc-sala-materias/header_materias.php");?>
     </header>
+    <main class="materias-win">
+        <div class="left-aside-materias">
+            <?php include("left-aside.php");?>
+        </div>
+        <div class="right-aside-materias">
+            <?php
+                switch (@$_REQUEST["pages-material"]) {
+                    case "programacao":
+                        include("programacao/programacao.php");
+                    break;  
+                    case "eletronica":
+                        include("eletronica/eletronica.php");
+                    break;
+                    case "devweb":
+                        include("dev_web/devweb.php");
+                    break;
+                    case "matematica":
+                        include("matematica/matematica.php");
+                    break;
+                    default:        
+                        print("Janela Materias");
+                    break;
+                }
+            ?>
+        </div>
+    </main>
 </body>
 </html>
 
@@ -18,23 +45,3 @@
 
 
 
-<?php 
-include("left-aside.php");
-
-switch (@$_REQUEST["pages-material"]) {
-    case "programacao":
-        include("programacao/programacao.php");
-    break;  
-    case "eletronica":
-        include("eletronica/eletronica.php");
-    break;
-    case "devweb":
-        include("dev_web/devweb.php");
-    break;
-    case "matematica":
-        include("matematica/matematica.php");
-    break;
-    default:        
-        print("Janela Materias");
-    break;
-}
---
+++ caracol/room/inc-sala-materias/header_materias.php
@@ -0,0 +1,23 @@
+
+<div class="mn-header">
+    <div class="container-hhd">
+        <div class="itm-cnt-hdd">
+            <div class="itm-hhd">
+                <div class="logo-sala">
+                    <a href="../../indexUpgrade.php">caracoLearn</a>
+                </div>
+            </div>
+            <nav class="itm-navbar">
+                <ul class="list-navbar">
+                    <li><a href="../sala-materias.php">Materias</a></li>
+                    <li><a href="">Biblioteca</a></li>
+                    <li><a href="">Ficheiros</a></li>
+                    <li><a href="">Notas</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="logn-main">
+            <p>profile</p>
+        </div>
+    </div>
+</div>
